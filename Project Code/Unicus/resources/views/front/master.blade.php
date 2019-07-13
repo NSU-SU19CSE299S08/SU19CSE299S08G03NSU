e9c04f9962a98ac8ecdec5c6f2c783877c9fc91e
majhar-nayem <majharul_nayem@yahoo.com>
2019-07-14 01:22:17 +0600

    New Feature #12: master file body added

--- Project Code/Unicus/resources/views/front/master.blade.php
+++ Project Code/Unicus/resources/views/front/master.blade.php
@@ -32,4 +32,41 @@
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+    @include("partials.header")
+    <!-- Left side column. contains the logo and sidebar -->
+   @include("partials.sidebar")
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Dashboard
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">Dashboard</li>
+            </ol>
+        </section>
+
+        <!-- Main content -->
+        @yield("content")
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    @include("partials.footer")
+
+    <!-- Control Sidebar -->
+    @include("partials.controll-sidebar")
+    <!-- /.control-sidebar -->
+    <!-- Add the sidebar's background. This div must be placed
+         immediately after the control sidebar -->
+    <div class="control-sidebar-bg"></div>
+
+</div>
+
 </html>
