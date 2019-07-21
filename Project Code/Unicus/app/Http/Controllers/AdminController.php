1edb5ac382742f32382e3db62f3b2cbeb40a831e
majhar-nayem <majharul_nayem@yahoo.com>
2019-07-21 21:39:51 +0600

    #18: Login credentials checking function added

--- Project Code/Unicus/app/Http/Controllers/AdminController.php
+++ Project Code/Unicus/app/Http/Controllers/AdminController.php
@@ -11,4 +11,22 @@ class AdminController extends Controller
         return view('admin.login');
     }
 
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email'=> 'required|email',
+            'password'=> 'required|min:8'
+        ]);
+
+
+        $credentials= ['email'=> $request->email, 'password'=> $request->password];
+
+        if(Auth::guard('admin')->attempt($credentials)){
+
+            return redirect()->intended(route('admin.dashboard'));
+        }
+
+        return redirect()->back()->withInput($request->only('email', 'password'));
+    }
+
 }
