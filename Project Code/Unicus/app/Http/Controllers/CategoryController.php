80d9dd3f06b502a3870acc0e7ecac7d1076b168b
majhar-nayem <majharul_nayem@yahoo.com>
2019-07-22 15:15:44 +0600

    Issue#22 showAddCategoryForm function added

--- Project Code/Unicus/app/Http/Controllers/CategoryController.php
+++ Project Code/Unicus/app/Http/Controllers/CategoryController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function showAddCategoryForm(){
+
+        return view('admin.category.add-category');
+    }
 }
