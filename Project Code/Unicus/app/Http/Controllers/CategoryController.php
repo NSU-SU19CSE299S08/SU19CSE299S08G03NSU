2726f1670014f9a34c8db54401eec800b9666228
majhar-nayem <majharul_nayem@yahoo.com>
2019-07-22 15:25:56 +0600

    Issue#22 saveCategoryInfo function added to save data in database

--- Project Code/Unicus/app/Http/Controllers/CategoryController.php
+++ Project Code/Unicus/app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -9,5 +10,17 @@ class CategoryController extends Controller
     public function showAddCategoryForm(){
 
         return view('admin.category.add-category');
+    }
+
+    public function saveCategoryInfo(Request $request){
+
+        $category = new Category();
+        $category->category_name = $request->category_name;
+        $category->category_description = $request->category_description;
+        $category->publication_status = $request->publication_status;
+        $category->save();
+
+        return redirect('category.add')->with('message', 'Category Info saved Successfully!');
+
     }
 }
