143b147870015aba0f2344d2e3349c7300207c55
majhar-nayem <majharul_nayem@yahoo.com>
2019-08-02 23:47:58 +0600

    Feature #28 : add-payment form created

--- Project Code/Unicus/resources/views/admin/partials/sidebar.blade.php
+++ Project Code/Unicus/resources/views/admin/partials/sidebar.blade.php
@@ -66,14 +66,14 @@
             <li class="treeview">
                 <a href="#">
                     <i class="fa fa-pie-chart"></i>
-                    <span>Products</span>
+                    <span>Payement</span>
                     <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a href="{{ route('payment.add') }}"><i class="fa fa-circle-o"></i> Add Product</a></li>
-                    <li><a href="{{ route('payment.manage') }}"><i class="fa fa-circle-o"></i>Manage Product</a></li>
+                    <li><a href="{{ route('payment.add') }}"><i class="fa fa-circle-o"></i> Add Payment</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Manage Product</a></li>
                 </ul>
             </li>
 
