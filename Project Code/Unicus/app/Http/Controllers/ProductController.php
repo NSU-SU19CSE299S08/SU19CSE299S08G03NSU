422bc067b77346d93f549bdc90f697045d6df5b5
majhar-nayem <majharul_nayem@yahoo.com>
2019-08-05 15:26:57 +0600

    Feature #29 : All product function created

--- Project Code/Unicus/app/Http/Controllers/ProductController.php
+++ Project Code/Unicus/app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -9,5 +10,12 @@ class ProductController extends Controller
     public function addProduct(){
 
         return view('admin.product.add-product');
+    }
+
+    public function manageProduct(){
+        $product = Product::orderBy('id', 'asc')->get();
+        return view('admin.product.manage-product', [
+            'products' => $product
+        ]);
     }
 }
