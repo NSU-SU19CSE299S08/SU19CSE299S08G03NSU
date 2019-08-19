357b0050665f605ce5d08e768303198993bc78c5
majhar-nayem <majharul_nayem@yahoo.com>
2019-08-19 11:56:29 +0600

    Issue #32 :saveBrandInfo function created to save brand info

--- Project Code/Unicus/app/Http/Controllers/BrandController.php
+++ Project Code/Unicus/app/Http/Controllers/BrandController.php
@@ -9,4 +9,14 @@ class BrandController extends Controller
     public function ShowAddBrandForm(){
         return view('brand.add-brand');
     }
+
+    public function saveBrandInfo(Request $request){
+        $brand = new Brand();
+        $brand->brand_name = $request->brand_name;
+        $brand->brand_description = $request->brand_description;
+        $brand->publication_status = $request->publication_status;
+        $brand->save();
+
+        return redirect('/brand/add-brand')->with('message', 'Brand info save successfully.');
+    }
 }
